0a3d00e341e9f66a48fab1a2bc5d89a0e9ef7528
Matt Wistrand <matt@troglodytidae.com>
2013-11-06 10:39:01 -0600

    Added custom fixtures; functionality to display modals on load

--- src/modal.js
+++ src/modal.js
@@ -1,9 +1,72 @@
-(function($, window, document, ns) {
+window.u$ || (window.u$ = {});
+
+(function($, window, document, ns, eventProto) {
+'use strict';
 
 ns || (ns = window);
 
-ns.modal = function() {
+var overlay,
+
+  defaults = {
+
+    /**
+     * Either the existing modal `$` elements, a selector to fetch
+     * existing modals from the DOM, or an HTML string that will be
+     * passed to `$`, or an array of data that will be passed to `$`
+     * to create a new modal when the AJAX data is fetched.
+     *
+        `modals: '.js-modal'`
+     * - OR -
+        `modals: $('.js-modal')`
+     * - OR -
+        `modals: ['div', {
+          'class': 'js-modal'
+        }]`
+     * - OR -
+        `modals: '<div class="modal"></div>'`
+     */
+    modals: null
+  },
   
+  initialize = function(triggers, options) {
+    this.options = $.extend({}, defaults, (options || null));
+
+    if (triggers === null) {
+      this.show(0);
+    } else {
+
+    }
+  },
+
+  modalProto = {
+    show: function(i) {
+      this.modals || (this.modals = $(this.options.modals));
+      this.modals.eq(i).removeClass('is-invisible');
+    }
+  },
+
+  ajaxModalProto = $.extend({}, modalProto, {
+
+  });
+
+ns.modal = function(triggers, options) {
+  var proto = modalProto,
+    instance;
+
+  if (!(triggers instanceof $)) {
+    options = triggers;
+    triggers = null;
+  }
+
+  if (options && options.url) {
+    proto = ajaxModalProto;
+  }
+
+  instance = Object.create(proto);
+
+  initialize.call(instance, triggers, options);
+
+  return instance;
 };
 
-})((window.Zepto || window.jQuery), window, document)
+})((window.Zepto || window.jQuery), window, document, u$);
--- test/spec/modalSpec.js
+++ test/spec/modalSpec.js
@@ -1,5 +1,16 @@
 describe('Zepto-Compatible jQuery Modal Box', function() {
-  it('Actually loads something', function() {
-    expect(true).toBe(true);
+  it('Immediately displays an in-page modal', function() {
+    var modalInstance,
+      modals;
+
+    loadFixtures('immediate.html');
+
+    modals = $('.js-modal');
+    modalInstance = u$.modal({
+      modals: modals
+    });
+
+    expect(modals.get(0)).not.toHaveClass('is-invisible');
+    expect(modals.get(1)).toHaveClass('is-invisible');
   });
 });
