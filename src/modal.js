53dd5667673081a1f9f0323ad8045c2d3fa04191
Matt Wistrand <matt@troglodytidae.com>
2013-11-06 12:54:41 -0600

    Added ability to get plain object without calling attach/show. Refactored  method.

--- src/modal.js
+++ src/modal.js
@@ -8,6 +8,13 @@ ns || (ns = window);
 var overlay,
 
   defaults = {
+    
+    /**
+     * The prefix for the event namespace. The namespace is generated
+     * as follows: `eventNamespace + 'Modal:' + name + 'Event'`
+     * For example, 'myAppModal:showEvent'.
+     */
+    eventNamespace: null,
 
     /**
      * Either the existing modal `$` elements, a selector to fetch
@@ -25,23 +32,68 @@ var overlay,
      * - OR -
         `modals: '<div class="modal"></div>'`
      */
-    modals: null
+    modals: null,
+
+    /**
+     *
+     */
+    triggerSelector: '.js-triggerModal'
   },
   
   initialize = function(triggers, options) {
     this.options = $.extend({}, defaults, (options || null));
 
-    if (triggers === null) {
-      this.show(0);
-    } else {
-
+    if (triggers || this.options.modals) {
+      if (triggers === null) {
+        this.show(null, 0);
+      } else {
+        this.triggers = triggers;
+        this.attach();
+      }
     }
   },
 
   modalProto = {
-    show: function(i) {
-      this.modals || (this.modals = $(this.options.modals));
-      this.modals.eq(i).removeClass('is-invisible');
+    attach: function() {
+      var ns = this.options.eventNamespace + 'Modal:showEvent',
+        sel = this.options.triggerSelector,
+        callback = $.proxy(function(e) {
+          var trigger = $(e.target);
+
+          if (!trigger.is(sel)) {
+            trigger = trigger.parent(sel).eq(0);
+          }
+
+          if (trigger.length) {
+            e.preventDefault();
+
+            this.show(trigger);
+          }
+        }, this);
+
+      this.triggers.on('click.' + ns, sel, callback);
+    },
+
+    loadModal: function(trigger, i) {
+      var index = trigger ? trigger.data('modalindex') : i,
+        modal;
+
+      if (typeof index !== 'number') {
+        index = this.modals && this.modals.length || 0;
+        modal = $('#' + trigger.data('modalid'));
+        this.modals = index ? this.modals.add(modal) : modal;
+        trigger.data('modalindex', index);
+      } else {
+        this.modals || (this.modals = $(this.options.modals));
+        modal = this.modals.eq(index);
+      }
+      return modal;
+    },
+
+    show: function(trigger, i) {
+      var modal = this.loadModal(trigger, i);
+
+      modal.removeClass('is-invisible');
     }
   },
 
--- test/spec/modalSpec.js
+++ test/spec/modalSpec.js
@@ -1,16 +1,53 @@
 describe('Zepto-Compatible jQuery Modal Box', function() {
-  it('Immediately displays an in-page modal', function() {
+
+  describe('Common Functionality', function() {
+    var instance;
+
+    beforeEach(function() {
+      instance = u$.modal();
+      loadFixtures('immediate.html');
+    });
+
+    it('Does not trigger anything on load', function() {
+      expect($('.js-modal')).toHaveClass('is-invisible');
+    });
+  });
+
+  describe('Immediately-invoked in-page modal', function() {
     var modalInstance,
       modals;
 
-    loadFixtures('immediate.html');
+    beforeEach(function() {
+      loadFixtures('immediate.html');
 
-    modals = $('.js-modal');
-    modalInstance = u$.modal({
-      modals: modals
+      modals = $('.js-modal');
+      modalInstance = u$.modal({
+        modals: modals
+      });
     });
 
-    expect(modals.get(0)).not.toHaveClass('is-invisible');
-    expect(modals.get(1)).toHaveClass('is-invisible');
+    it('Unhides the first modal', function() {
+      expect(modals.eq(0)).not.toHaveClass('is-invisible');
+    });
+
+    it('Keeps other in-page modals hidden.', function() {
+      expect(modals.eq(1)).toHaveClass('is-invisible');
+    });
+  });
+
+  describe('Triggered in-page modal', function() {
+    var modalInstance;
+
+    beforeEach(function() {
+      loadFixtures('static.html');
+
+      modalInstance = u$.modal($(document.body));
+    });
+
+    it('Displays the appropriate modal when a trigger is clicked', function() {
+      $('.js-triggerModal').eq(0).trigger('click');
+
+      expect($('.js-modal').eq(0)).not.toHaveClass('is-invisible');
+    });
   });
 });
